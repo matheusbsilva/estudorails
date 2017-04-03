17a6ce9cbb4e3fc8d642a32a7f2449ec1373ed3c
matheusbsilva <matheusbattista@hotmail.com>
2017-04-03 22:22:37 +0000

    testes de usuario

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170403105035) do
+ActiveRecord::Schema.define(version: 20170403221131) do
 
   create_table "alumns", force: :cascade do |t|
     t.integer  "registry_alumn"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20170403105035) do
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
